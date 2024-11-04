2923bf036b0e4d74d9774f4ffdf9e821b47fe5c0
Nicholas Gallina <nicholasgallina@Nicholas-Gallinas-MacBook-Air.local>
2024-11-04 14:24:17 -0500

    added a main method :)

---
+++ src/model/Main.java
@@ -0,0 +1,16 @@
+// Main.java
+
+// Define the package if needed, otherwise leave it out
+// package your.package.name;
+
+public class Main {
+
+    // The main method: entry point of the Java application
+    public static void main(String[] args) {
+        // Print a message to test that the program runs
+        System.out.println("This will eventually be chess.");
+
+        // You can add more functionality here
+    }
+}
+
